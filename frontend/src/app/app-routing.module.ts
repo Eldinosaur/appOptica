2137ac37bda327fddd3ba235abc65e16b72f3795
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2022-07-27 16:19:46 -0500

    implementacion del servicio y paginas de pacientes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,8 +8,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'pacientes',
     pathMatch: 'full'
+  },
+  {
+    path: 'pacientes',
+    loadChildren: () => import('./pacientes/pacientes.module').then( m => m.PacientesPageModule)
   },
 ];
 
