9fcf9e895f1187c4dfc1d1c70c7a422f39d06c59
Eldinosaur <82054019+Eldinosaur@users.noreply.github.com>
2022-07-28 09:17:11 -0500

    mostrar y eliminar

--- frontend/src/app/pacientes/pacientes.page.ts
+++ frontend/src/app/pacientes/pacientes.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PacientesService } from '../services/pacientes.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pacientes',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pacientes.page.scss'],
 })
 export class PacientesPage implements OnInit {
+  pacientes: any = []
 
-  constructor() { }
+  constructor(private pacientesService:PacientesService, private alertController:AlertController) { }
 
-  ngOnInit() {
+  ngOnInit():void {
+    this.cargarPacientes()
+  }
+  ionViewWillEnter(){
+    this.cargarPacientes()
+  }
+
+  cargarPacientes(){
+    this.pacientesService.obtenerPacientes().subscribe(res =>{
+      this.pacientes = res
+      console.log(res)
+    },err => console.log(err))
+  }
+  async eliminarCurso(id){
+    const alert = await this.alertController.create({
+      header:"Eliminar Paciente",
+      message:"Desea eliminar el paciente?",
+      buttons:[
+        {
+          text:'Ok',
+          handler:()=>{
+            this.pacientesService.eliminarPaciente(id).subscribe(res => this.cargarPacientes(),err => console.log(err));
+          },
+        },'Cancelar',
+      ],
+    });
+    alert.present()    
   }
 
 }
--- frontend/src/app/services/pacientes.service.ts
+++ frontend/src/app/services/pacientes.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PacientesService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  API = "http://localhost:1337/tbl-pacientes"
+
+  obtenerPacientes(){ 
+    return this.http.get(this.API)
+   }
+  
+  obtenerPaciente(id:string){
+    return this.http.get(`${this.API}/${id}`)
+  }
+
+  insertarPaciente(nombre:string,apellido:string,cedula:string,direccion:string,telefono:string){
+    return this.http.post(this.API,{
+      nombre,apellido,cedula,direccion,telefono
+    })
+  }
+
+  actualizarPaciente(id:string,nombre:string,apellido:string,cedula:string,direccion:string,telefono:string){
+    return this.http.put(`${this.API}/${id}`,{
+      nombre,apellido,cedula,direccion,telefono
+    })
+  }
+
+  eliminarPaciente(id:string){
+    return this.http.delete(`${this.API}/${id}`)
+  }
 }
